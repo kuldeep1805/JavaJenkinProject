22899a3d1bdddde86b6fe8aa96b428713a9e06db
kuldeep1805 <kuldeep1805sharma@gmail.com>
2021-07-29 16:28:49 +0530

    second

--- JenkinJavaProject/src/main/java/com/lti/entity/UserApp.java
+++ JenkinJavaProject/src/main/java/com/lti/entity/UserApp.java
@@ -3,5 +3,7 @@ package com.lti.entity;
 public class UserApp {
 public static void main(String[] args) {
 	System.out.println("Welcome to jenkins");
+	System.out.println("Jenkins is working");
+	
 }
 }
